556cb62fcf0ad62fe307bb5efb968ebf8c5da528
labaffa <dellarovere.lante@gmail.com>
2024-05-10 21:43:05 +0200

    Add db stuff

---
+++ fews2board/db/db_setup.py
@@ -0,0 +1,44 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from fews2board import config
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+ASYNC_SQLALCHEMY_DATABASE_URL = config.PG_ASYNC_URL
+SQLALCHEMY_DATABASE_URL = config.PG_URL
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    future=True,
+    pool_pre_ping=True
+)
+SessionLocal = sessionmaker(
+    engine, 
+    expire_on_commit=False, 
+    autocommit=False, 
+    autoflush=False
+)
+async_engine = create_async_engine(
+    ASYNC_SQLALCHEMY_DATABASE_URL, 
+    future=True,
+    pool_pre_ping=True
+)
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False, 
+    autocommit=False, autoflush=False
+ )
+
+Base = declarative_base()
+
+
+async def get_async_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ fews2board/db/models/__init__.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+from fews2board import config
+
+
+Base = declarative_base(metadata=MetaData(schema=config.PG_SCHEMA_NAME))
+
+
+class MyBase(Base):
+    __abstract__ = True
+
+    def to_dict(self):
+        return {field.name:getattr(self, field.name) for field in self.__table__.c}
+
+
+from fews2board.db.models.telegram import TgMessage, TgChannel, TgSentiment, TgTopicId
+from fews2board.db.models.luigi import MarkerTable
+from fews2board.db.models.media_monitoring import MCStory, MCSentiment, MCTopicId
+from fews2board.db.models.framework import Topic, Date, Country, Domain
+from fews2board.db.models.facts import TopicIdDayAggTg, TopicIdDayDomainAggTg, \
+    TgSentimentDayAgg, TgSentimentDayDomainAgg, MCSentimentDayAgg, MCSentimentDayDomainAgg, \
+    MCTopicIdDayAgg, MCTopicIdDayDomainAgg, TgTopicIdCountDayAgg, \
+    TgTopicIdCountDayDomainAgg

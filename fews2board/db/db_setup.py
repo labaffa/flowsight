3bd621d276b71d0d79fc6805cc7951b2664a01cc
labaffa <dellarovere.lante@gmail.com>
2024-05-16 14:35:07 +0200

    comment psycopg2 parts

--- fews2board/db/db_setup.py
+++ fews2board/db/db_setup.py
@@ -4,41 +4,41 @@ from fews2board import config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-ASYNC_SQLALCHEMY_DATABASE_URL = config.PG_ASYNC_URL
-SQLALCHEMY_DATABASE_URL = config.PG_URL
+# ASYNC_SQLALCHEMY_DATABASE_URL = config.PG_ASYNC_URL
+# SQLALCHEMY_DATABASE_URL = config.PG_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    future=True,
-    pool_pre_ping=True
-)
-SessionLocal = sessionmaker(
-    engine, 
-    expire_on_commit=False, 
-    autocommit=False, 
-    autoflush=False
-)
-async_engine = create_async_engine(
-    ASYNC_SQLALCHEMY_DATABASE_URL, 
-    future=True,
-    pool_pre_ping=True
-)
-AsyncSessionLocal = sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False, 
-    autocommit=False, autoflush=False
- )
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, 
+#     future=True,
+#     pool_pre_ping=True
+# )
+# SessionLocal = sessionmaker(
+#     engine, 
+#     expire_on_commit=False, 
+#     autocommit=False, 
+#     autoflush=False
+# )
+# async_engine = create_async_engine(
+#     ASYNC_SQLALCHEMY_DATABASE_URL, 
+#     future=True,
+#     pool_pre_ping=True
+# )
+# AsyncSessionLocal = sessionmaker(
+#     async_engine, class_=AsyncSession, expire_on_commit=False, 
+#     autocommit=False, autoflush=False
+#  )
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
-async def get_async_db():
-    async with AsyncSessionLocal() as db:
-        yield db
-        await db.commit()
+# async def get_async_db():
+#     async with AsyncSessionLocal() as db:
+#         yield db
+#         await db.commit()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()

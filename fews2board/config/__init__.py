37f4bce723492e4338faf4b342566c524e63d705
Giosuè Ruscica <dellarovere.lante@gmail.com>
2024-09-02 12:46:02 +0200

    Update FEWS_COUNTRIES

--- fews2board/config/__init__.py
+++ fews2board/config/__init__.py
@@ -26,25 +26,42 @@ PG_PASSWORD = os.getenv("PG_PASSWORD")
 PG_URL = f'postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DATABASE}'
 PG_ASYNC_URL = f'postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DATABASE}'
 PG_SCHEMA_NAME = os.getenv("PG_SCHEMA_NAME")
-FEWS_COUNTRIES = [
-    "af",
-    "ao",
-    "bj",
-    "bf",
-    "bi",
-    "cm",
-    "td",
-    "sv",
-    "et",
-    "gt",
-    "ht",
-    "ke",
-    "ly",
-    "mz",
-    "ng",
-    "sn",
-    "tg",
-    "ve",
-    "zw"
-]
 
+FEWS_COUNTRIES_FROM_ETL = {
+    "af": {"telecatch_collection": "afghanistan", "mediacloud_collection": 38376339},
+    "ao": {"telecatch_collection": "angola", "mediacloud_collection": 34412237},
+    "bj": {"telecatch_collection": "benin", "mediacloud_collection": 34412177},
+    "bf": {"telecatch_collection": "burkina faso", "mediacloud_collection": 34412047},
+    "bi": {"telecatch_collection": "burundi", "mediacloud_collection": 38379378},
+    "cm": {"telecatch_collection": "cameroon", "mediacloud_collection": 38379387},
+    "td": {"telecatch_collection": "chad", "mediacloud_collection": 38379436},
+    "dj": {"telecatch_collection": "djibouti", "mediacloud_collection": 34412350},
+    "cd": {"telecatch_collection": "drc", "mediacloud_collection": 34412042},
+    "sv": {"telecatch_collection": "el salvador", "mediacloud_collection": 34412288},
+    "et": {"telecatch_collection": "ethiopia", "mediacloud_collection": 34412034},
+    "gh": {"telecatch_collection": "ghana", "mediacloud_collection": 34412202},
+    "gt": {"telecatch_collection": "guatemala", "mediacloud_collection": 34412063},
+    "ht": {"telecatch_collection": "haiti", "mediacloud_collection": 34412303},
+    "ci": {"telecatch_collection": "ivory coast", "mediacloud_collection": 34412173},
+    "ke": {"telecatch_collection": "kenya", "mediacloud_collection": 34412126},
+    "lb": {"telecatch_collection": "lebanon", "mediacloud_collection": 34412343},
+    "ly": {"telecatch_collection": "libya", "mediacloud_collection": 38380279},
+    "mg": {"telecatch_collection": "madagascar", "mediacloud_collection": 34412370},
+    "mw": {"telecatch_collection": "malawi", "mediacloud_collection": 34412402},
+    "ml": {"telecatch_collection": "mali", "mediacloud_collection": 34412222},
+    "mr": {"telecatch_collection": "mauritania", "mediacloud_collection": 34412134},
+    "mz": {"telecatch_collection": "mozambique", "mediacloud_collection": 34412248},
+    "ni": {"telecatch_collection": "nicaragua", "mediacloud_collection": 34412113},
+    "ng": {"telecatch_collection": "nigeria", "mediacloud_collection": 38376341},
+    "rw": {"telecatch_collection": "rwanda", "mediacloud_collection": 34412053},
+    "sn": {"telecatch_collection": "senegal", "mediacloud_collection": 38380807},
+    "so": {"telecatch_collection": "somalia", "mediacloud_collection": 34412155},
+    "sd": {"telecatch_collection": "sudan", "mediacloud_collection": 34412379},
+    "tj": {"telecatch_collection": "tajikistan", "mediacloud_collection": 34412129},
+    "tz": {"telecatch_collection": "tanzania", "mediacloud_collection": 34412085},
+    "tg": {"telecatch_collection": "togo", "mediacloud_collection": 34412192},
+    "ve": {"telecatch_collection": "venezuela", "mediacloud_collection": 34412387},
+    "zm": {"telecatch_collection": "zambia", "mediacloud_collection": 34412396},
+    "zw": {"telecatch_collection": "zimbabwe", "mediacloud_collection": 34412406}
+}
+FEWS_COUNTRIES = list(FEWS_COUNTRIES_FROM_ETL.keys())
